b20f497d84677be8aae846b3b3b4c3bbdc5b9b20
TnTo <tnto@hotmail.it>
2018-04-26 22:38:32 +0200

    Hanaby Repo

---
+++ Hanabi.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+class tessera: {
+    number = 0
+    color = ""
+    owner = ""
+
+    def getnumber (self):
+        return
+    def getcolor (self):
+        return
+    def getowner (self):
+        return
+    def setowner (self):
+        return
+    def __init__ (self, num, col):
+        return
+}
