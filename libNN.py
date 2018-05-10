cac728faf5df392e6afc3a584dc3737e437661bd
TnTo <tnto@hotmail.it>
2018-05-11 01:56:17 +0200

    Start to writing NN, still missing learning

--- libNN.py
+++ libNN.py
@@ -1,5 +1,6 @@
-from numpy import array, matrix, zeros, empty
+from numpy import array, matrix, zeros, empty, exp
 from numpy.random import randn
+from random import uniform as randu
 
 #Hyperparameters
 NInput = 83
@@ -16,6 +17,7 @@ class NeuralNetwork:
         #matrix of weight from input to first layer
         self.w = [] #list of weight matrix
         self.b = [] #list of bias vector
+        #Need i/o from file to store improvements
         for i in range (0, (NHiddenLayers + 1)):
             rows = NeuronsHiddenLayer
             columns = NeuronsHiddenLayer
@@ -27,6 +29,8 @@ class NeuralNetwork:
             b.append(zeros(columns, 1)) #Zeros
         self.moves = []
         self.lastmove = []
+        self.players = []
+        self.activeplayer = ""
 
     def parsestatus (self, status):
         input = empty(NInput)
@@ -42,11 +46,12 @@ class NeuralNetwork:
         #Opponents' hand
         #-1 means None
         #Colors are [0,4] instead of [1,5]
-        players = list(status["players"].keys())
-        ind = players.index(status["activeplayer"])
+        self.players = list(status["players"].keys())
+        ind = self.players.index(status["activeplayer"])
+        self.activeplayer = status["activeplayer"]
         for i in range (0,3):
             for j in range (0,4):
-                a, b, c, d = status["players"][players[(ind + 1 + i) % 4]]["hand"][j]
+                a, b, c, d = status["players"][self.players[(ind + 1 + i) % 4]]["hand"][j]
                 first = 23 + i*16 + j*4
                 if a == None:
                     input[first] = -1
@@ -58,14 +63,13 @@ class NeuralNetwork:
                     input[first + 1] = colors.index(b)
                 if c == None:
                     input[first + 2] = -1
-                elif:
-                    c == True:
+                elif c == True:
                     input[first + 2] = 1
                 else:
                     input[first + 2] = 0
                 if d == None:
                     input[first + 3] = -1
-                elif:
+                elif d == True:
                     input[first + 3] = 1
                 else:
                     input[first + 3] = 0
@@ -94,18 +98,47 @@ class NeuralNetwork:
         return Input
 
     def parseoutput (self, output):
-        pass
+        if output in range (0,4):
+            return [0, output]
+        elif output in range (4,8):
+            return [1, output - 4]
+        elif output in range (8,35):
+            player = ((output - 8) // 9) + 1
+            if ((output - 8) % 9) in range(0,5):
+                return [2, (((output - 8) % 9) + 1), \
+                    self.players[(self.players.index(self.activeplayer) + player) % 4]]
+            elif ((output - 8) % 9) in range(5,9):
+                return [2, colors[(((output - 8) % 9) - 5)], \
+                    self.players[(self.players.index(self.activeplayer) + player) % 4]]
+
+    def sigma (self, x):
+        return 1/(1 + exp(-x))
+
+    def softmax (self, x):
+        y = exp (x)
+        return y/sum(y)
+
+    def goforward(self, input):
+        input = input #Maybe unnecessary
+        for i in range(0, NHiddenLayers + 1):
+            if i = NHiddenLayers:
+                output = self.softmax((input @ self.w[i]) + self.b[i])
+            else:
+                output = self.sigma((input @ self.w[i]) + self.b[i])
+            input = output
+        return output
 
     def getinput (self, status):
         if status["last"] and self.lastmove != []:
             self.moves.append(self.lastmove)
             self.lastmove = []
         input = self.parsestatus(status)
-
-
-        output, outputprob = [0, 0.0]
+        outputprob = self.goforward(input)
+        r = randu(0.0,1.0)
+        output = 0
+        while (sum(outputprob[:(output + 1)]) < r):
+            output = output + 1
         self.lastmove = [input, output, outputprob]
-
         return self.parseoutput(output)
 
     def update (self, points):
