01d192c8efdd1a27266bfda647828a66989f9739
TnTo <tnto@hotmail.it>
2018-05-14 20:50:36 +0200

    last commit after last commit

--- Hanabi.py
+++ Hanabi.py
@@ -7,15 +7,15 @@ from libNN import NeuralNetwork
 import matplotlib.pyplot as plt
 from numpy import polyfit
 
-#Input = inputsource()
-Input = NeuralNetwork()
+Input = inputsource()
+#Input = NeuralNetwork()
 
 InputList = [Input, Input, Input, Input]
 
 pointsmemory = []
 movesmemory = []
 
-NIter = 2
+NIter = 1
 
 for i in range (0, NIter):
     print("Match number " + str(i + 1) + " of " + str(NIter))
@@ -28,17 +28,17 @@ for i in range (0, NIter):
 
     pointsmemory.append(points)
 
-    movesmemory.append(Input.getnumberofmoves())
+    #movesmemory.append(Input.getnumberofmoves())
 
-    Input.update(points)
+    #Input.update(points)
 
 plt.scatter(range(0, NIter), pointsmemory)
 fit = polyfit (range(0, NIter), pointsmemory, deg=1)
 plt.plot(range(0, NIter), fit[0] * range(0, NIter) + fit[1], color='red')
 plt.figure()
 
-plt.scatter(range(0, NIter), movesmemory)
-fit = polyfit (range(0, NIter), movesmemory, deg=1)
-plt.plot(range(0, NIter), fit[0] * range(0, NIter) + fit[1], color='green')
+# plt.scatter(range(0, NIter), movesmemory)
+# fit = polyfit (range(0, NIter), movesmemory, deg=1)
+# plt.plot(range(0, NIter), fit[0] * range(0, NIter) + fit[1], color='green')
 
 plt.show()
