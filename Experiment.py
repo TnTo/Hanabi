32c0a62b4b9abda6a78e84527aa7d8050a319ebc
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2020-10-23 11:46:03 +0200

    memories

--- Experiment.py
+++ Experiment.py
@@ -162,9 +162,9 @@ class Experiment:
         if self.keep_memory == 0:
             self.memories.clear()
         elif len(self.memories) > self.keep_memory:
-            self.memories = self.memories.sort(
-                key=lambda m: m.post.score(), reverse=True
-            )[0 : self.keep_memory]
+            self.memories = sorted(
+                self.memories, key=lambda m: m.post.score(), reverse=True
+            )[0:self.keep_memory]
         self.update_nn()
         self.games = [Game(self.nn) for _ in range(self.n_games)]
 
